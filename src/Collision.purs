c7a256e078bb46992cbf680f566efa8a7a5054fe
Mateusz Curylo <mhcurylo@gmail.com>
2018-02-25 15:19:17 +0100

    Collisions

--- src/Collision.purs
+++ src/Collision.purs
@@ -19,17 +19,21 @@ data RelativeMotion = RelativeMotion {
   , direction :: Vector  
 }
 
-data Direction = L | R | T | B
+data Direction = T | R | B | L
 
 didCollide :: Physical -> Physical -> Maybe Direction
-didCollide a b = if getX mTop <= 0.0  
-                   && getY mTop >= 0.0
-                   && getX mBottom >= 0.0 
-                   && getY mBottom <= 0.0 
-  then Just L 
+didCollide a b = if tx <= 0.0  
+                   && ty >= 0.0
+                   && bx >= 0.0 
+                   && by <= 0.0 
+  then Just $ minimumBy  $ zip [ty, bx, by, ty] [T, R, B, L] 
   else Nothing
   where
   (MinkowskiDifference {mTop, mBottom}) = minkowskiDifference a b      
+  ty = getX mTop 
+  tx = getY mTop
+  bx = getX mBottom 
+  by = getY mBottom  
 
 
 center :: Physical -> Vector
@@ -44,6 +48,6 @@ relativeMotion a b = RelativeMotion {source, direction}
 minkowskiDifference :: Physical -> Physical -> MinkowskiDifference
 minkowskiDifference a b = MinkowskiDifference {mTop, mSize, mBottom}
   where
-  mTop = subV a.pos (addV b.pos b.size)
-  mSize = addV a.size b.size
-  mBottom = addV mTop mSize 
+  mTop = a.pos - (b.pos + b.size)
+  mSize = a.size + b.size
+  mBottom = mTop + mSize 
