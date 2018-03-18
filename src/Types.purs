4bd536e45b85bdc0e4064958f869fc368ff07b12
Arunas Cesonis <karunce@gmail.com>
2018-03-18 16:08:51 +0000

    Add name to Physical

--- src/Types.purs
+++ src/Types.purs
@@ -63,7 +63,8 @@ instance ringVector :: Ring Vector where
   sub = subV
 
 type Physical = {
-    pos  :: Vector
+    name :: String
+  , pos  :: Vector
   , vel  :: Vector
   , acc  :: Vector
   , size :: Vector
@@ -98,23 +99,24 @@ data Game = Start
   | Progress GameState
   | Finish Int Int 
 
-createPhysical :: Number -> Number -> Number -> Number -> Number -> Number -> Physical
-createPhysical ix iy w h x y = ({
-    pos: vec x y
+createPhysical :: String -> Number -> Number -> Number -> Number -> Number -> Number -> Physical
+createPhysical name ix iy w h x y = ({
+    name: name
+  , pos: vec x y
   , acc: zeroVector
   , vel: zeroVector
   , size: vec w h
   , inertia: vec ix iy
 }) 
 
-createBox :: Number -> Number -> Number -> Number -> Physical
-createBox = createPhysical 0.0 0.0
+createBox :: String -> Number -> Number -> Number -> Number -> Physical
+createBox name = createPhysical name 0.0 0.0
 
-createPaddle :: Number -> Number -> Paddle
-createPaddle = createPhysical 0.0 0.95 20.0 70.0
+createPaddle :: String -> Number -> Number -> Paddle
+createPaddle name = createPhysical name 0.0 0.95 20.0 70.0
 
-createBall :: Number -> Number -> Ball
-createBall = createPhysical 1.001 1.001 15.0 15.0
+createBall :: String -> Number -> Number -> Ball
+createBall name = createPhysical name 1.001 1.001 15.0 15.0
 
-createWall :: Number -> Wall
-createWall = createBox 960.0 5.0 20.0
+createWall :: String -> Number -> Wall
+createWall name = createBox name 960.0 5.0 20.0
